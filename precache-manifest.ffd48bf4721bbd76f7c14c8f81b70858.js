ca0a3ee7c555626c8ff07a5e94b900cc96600e67
jam42 <radiosilence42@gmail.com>
2019-04-23 17:30:32 +0600

    Updates

---
+++ precache-manifest.ffd48bf4721bbd76f7c14c8f81b70858.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e55fb179e54ee4ef488",
+    "url": "/test-app/static/css/main.18c570cd.chunk.css"
+  },
+  {
+    "revision": "0e55fb179e54ee4ef488",
+    "url": "/test-app/static/js/main.1097971c.chunk.js"
+  },
+  {
+    "revision": "c3af9d7e88abdfb8c56c",
+    "url": "/test-app/static/js/runtime~main.f4fa62e4.js"
+  },
+  {
+    "revision": "24be22ea5d36a297f7a4",
+    "url": "/test-app/static/js/2.08468393.chunk.js"
+  },
+  {
+    "revision": "c389813b3b1073c544d1c95474824c31",
+    "url": "/test-app/index.html"
+  }
+];
